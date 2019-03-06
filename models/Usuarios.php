793e4b6e985bef4ec0ca6ce15da52887adb9f544
jose maria gallego martel <josema.g.m.95@hotmail.com>
2019-03-06 17:15:50 +0100

    Carga Inicial

---
+++ models/Usuarios.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace app\models;
+
+/**
+ * This is the model class for table "usuarios".
+ *
+ * @property int $id
+ * @property string $nombre
+ * @property string $password
+ *
+ * @property Reservas[] $reservas
+ */
+class Usuarios extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'usuarios';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['nombre', 'password'], 'required'],
+            [['nombre', 'password'], 'string', 'max' => 255],
+            [['nombre'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'nombre' => 'Nombre',
+            'password' => 'Contraseña',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getReservas()
+    {
+        return $this->hasMany(Reservas::className(), ['usuario_id' => 'id'])->inverseOf('usuario');
+    }
+}
